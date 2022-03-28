01e4fc0f2737534c666f9674ac22ce1df903120c
Xing Di (底兴) <9487284+DetionDX@users.noreply.github.com>
2022-03-28 14:17:48 -0400

    Add files via upload

--- AttributeEditing/demo_stgan.py
+++ AttributeEditing/demo_stgan.py
@@ -22,8 +22,7 @@ import torchvision.transforms.functional as TF
 from FaceBoxes import FaceBoxes
 from TDDFA import TDDFA
 from utils.render import render
-# from util import tensor2im
-from utils.functions import cv_draw_landmark, cv_draw_landmark_only, get_suffix, crop_img, tensor2im, inpaint_img, color_transfer
+from utils.functions import get_suffix, crop_img, tensor2im
 from models.stgan import Generator
 
 
@@ -37,14 +36,11 @@ def main(args):
 
     align_crop = cropper.align_crop_opencv
 
-    # face_factor = 0.5
     face_factor = 0.5
     align_type = 'similarity'  # choices=['affine', 'similarity']
     order = 3 # 'The order of interpolation.' choices=[0, 1, 2, 3, 4, 5]
     mode = 'edge' # ['constant', 'edge', 'symmetric', 'reflect', 'wrap']
     attrs = ["Bangs", "Blond_Hair", "Brown_Hair", "Bushy_Eyebrows", "Eyeglasses", "Male", "Mouth_Slightly_Open", "Mustache", "Pale_Skin", "Young"]
-    # input_label = torch.Tensor([0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
-    # input_label = torch.eye(len(attrs))
     input_label = torch.Tensor([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                                 [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
                                 [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
@@ -77,9 +73,6 @@ def main(args):
 
     # define a video capture object
     vid = cv2.VideoCapture(0)
-    # vid = cv2.VideoCapture('/Users/xingdi/netmind-face/3DDFA_V2/cropped_align_celeba_0.5/%06d.jpg')
-    # vid = cv2.VideoCapture('/Users/xingdi/Desktop/Obama.webm')
-    # vid = cv2.VideoCapture('/Users/xingdi/Desktop/webcam_xing_0214_noglasses.mov')
     fc = 0
 
     dense_flag = args.opt in ('3d',)
@@ -90,15 +83,9 @@ def main(args):
     while(True):
 
 
-        
-        # print(fc)
-        # Capture the video frame
-        # by frame
+    
         ret, img = vid.read()
 
-        # print(np.any(img))
-        # if np.any(img):
-        #     continue
 
         frame_bgr = img.copy()
 
@@ -157,17 +144,6 @@ def main(args):
         pre_ver = ver
 
 
-            # print(ver.shape)
-            # print(ld.shape)
-
-        # if args.opt == '2d_sparse_only':
-        #     res = cv_draw_landmark_only(frame_bgr, ld)
-        # elif args.opt == '2d_sparse':
-        #     res = cv_draw_landmark(frame_bgr, ver)
-        # elif args.opt == '3d':
-        #     res = render(frame_bgr, [ver], tddfa.tri, alpha=1.0, with_bg_flag=False)
-        # else:
-        #     raise ValueError(f'Unknown opt {args.opt}')
 
 
 
@@ -186,24 +162,18 @@ def main(args):
 
         
         img_id = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB)
-        # print(img_id.shape)
+
         img_id = trans(Image.fromarray(img_id)).unsqueeze(0)
 
-        # print(img_id.shape)
+
         img_o  = netG(img_id.repeat((input_label.shape[0], 1,1,1 )), input_label)
 
-        # img_o = tensor2ims(img_o)
-        # print(torch.cat(img_o,img_id), 0))
         img_o = make_grid(torch.cat((img_id, img_o), 0), nrow = len(attrs) + 1, normalize=False)
         img_o = img_o.numpy()
-        # print(img_o.shape)
-        # print(np.max(img_o))
+
 
         img_o = (np.transpose(img_o, (1, 2, 0)) + 1) / 2.0 * 255.0
         img_o = img_o.astype(np.uint8)
-        # print(np.max(img_o))
-        # print(img_o.shape)
-        # print(np.max(img_o))
 
         img_o = cv2.cvtColor(img_o, cv2.COLOR_RGB2BGR)
 
